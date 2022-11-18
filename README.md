b170291c0ae0e89b2c96635c1e19aaff9d0192fc
Jessicamf4 <jessicamarquesfialho0@gmail.com>
2022-11-18 20:20:36 -0300

    Validação de login

---
+++ README.md
@@ -0,0 +1,88 @@
+# CREATE
+
+sql = "INSERT INTO pessoa (nome, email, senha)  VALUES (%s, %s, %s) "
+
+data = (
+    "Jéssica",
+    "jess@gmail.com",
+    "abcd"
+)
+
+cursor.execute(sql, data)
+con.commit()
+
+
+#READ
+
+sql = "SELECT * FROM pessoa"
+
+cursor.execute(sql)
+results = cursor.fetchall()
+
+#função fetchall para recuperar todos os resultados da query SQL
+
+for result in results:
+    print(result)
+
+
+#UPDATE
+
+#sql = "UPDATE users SET name = %s, email = %s WHERE nome = %s"
+#data = (
+   # 'Jéssica Marques',
+   # 'jesss@teste.com.br',
+   # "Jéssica"
+#)
+
+#cursor.execute(sql, data)
+#con.commit()
+
+#recordsaffected = cursor.rowcount
+
+#DELETE
+
+#sql = "DELETE FROM users WHERE id = %s"
+#data = (2,)
+
+#cursor.execute(sql, data)
+#con.commit()
+
+
+
+
+
+
+## INSERIR UM USUÁRIO
+
+def insert_user(nome,email, senha):
+    try:
+        cursor = self.connection.cursor()
+        cursor.execute("""
+
+            INSERT INTO pessoa(nome, email, senha) VALUES (?, ?, ?)
+
+        """(nome, email, senha))
+        self.connection.commit()
+
+    except AttributeError:
+        print("faça a conexão")
+
+
+## VALIDAÇÃO DE LOGIN
+
+def check_user(email,senha):
+    try:
+        cursor = self.connection.cursor()
+        cursor.execute("""
+            SELECT * FROM pessoa;
+        """)
+
+        for linha in cursor.fetchall():
+            if linha[2].upper() == email.upper() and linha[3]== senha:
+                return "Usuario"
+                break
+            else:
+                continue
+        return "Sem acesso"
+    except:
+        pass
--- main.py
+++ main.py
@@ -1,66 +1,72 @@
-import mysql.connector
 
-con = mysql.connector.connect(host='localhost', database='cadastro', user='jess', password='Je140404')
+import mysql.connector
+from mysql.connector import Error
+
+con = mysql.connector.connect(host='localhost', database='cadastro', user='jess', password='')
 
 if con.is_connected():
     db_info = con.get_server_info()
-    print("Conctado!", db_info)
+    print("Conectado!", db_info)
 
 cursor = con.cursor()
 
-# CREATE
-
-sql = "INSERT INTO pessoa (nome, email, senha)  VALUES (%s, %s, %s) "
-
-data = (
-    "Jéssica",
-    "jess@gmail.com",
-    "abcd"
-)
-
-cursor.execute(sql, data)
-con.commit()
-
-
-#READ
-
-sql = "SELECT * FROM pessoa"
-
-cursor.execute(sql)
-results = cursor.fetchall()
-
-#função fetchall para recuperar todos os resultados da query SQL
-
-for result in results:
-    print(result)
-
-
-#UPDATE
-
-#sql = "UPDATE users SET name = %s, email = %s WHERE nome = %s"
-#data = (
-   # 'Jéssica Marques',
-   # 'jesss@teste.com.br',
-   # "Jéssica"
-#)
-
-#cursor.execute(sql, data)
-#con.commit()
-
-#recordsaffected = cursor.rowcount
-
-#DELETE
-
-#sql = "DELETE FROM users WHERE id = %s"
-#data = (2,)
-
-#cursor.execute(sql, data)
-#con.commit()
 
 recordsaffected = cursor.rowcount
 
 cursor.close()
 con.close()
 
-print(data)
-print(sql)
+## VALIDAÇÃO DE LOGIN
+
+
+
+loginusuario = input("Você deseja: 1 - realizar cadastro , 2 - realizar login")
+print("escolha" +loginusuario)
+
+if loginusuario == "1":
+    nomeCad = input("Digite seu nome:")
+    emailCad = input("Digite seu email: ")
+    senhaCad = input("Digite sua senha: ")
+    dados = '\''+ nomeCad +'\'' + ',\''+ emailCad +'\','+ '\'' + senhaCad +'\'' + ')'
+    print(dados)
+
+    declaracao = """INSERT INTO pessoa 
+    (nome, email, senha)
+    VALUES ("""
+    sql = declaracao + dados
+    print(sql)
+    try:
+        con = mysql.connector.connect(host='localhost', database='cadastro', user='jess', password='')
+    
+        inserirCadastro = sql
+        cursor = con.cursor()
+        cursor.execute(inserirCadastro)
+        con.commit()
+        print(cursor.rowcount, "Registros inseridos na tabela!")
+        cursor.close()
+    except Error as erro:
+        print("Falha ao inserir dados na tabela: { }".format(erro))
+
+
+else:
+    nomeLog = input("Digite seu nome: ")
+    senhaLog = input("Digite sua senha: ")
+    con = mysql.connector.connect(host='localhost', database='cadastro', user='jess', password='')
+    try:
+        cursor = con.cursor()
+        cursor.execute("SELECT senha FROM cadastro.pessoa WHERE nome LIKE '{}'".format(nomeLog))   
+        senha = cursor.fetchall()
+        print(senha[0][0])
+        cursor.close()
+    except Error as erro:
+        print("Erro {}".format(erro))
+    if senhaLog == senha[0][0]:
+        print("Sua senha está correta! Entrando...")
+    else:
+        print("Login e/ou senha incorretos..")
+
+
+    
+
+
+
